395ef80aace0954ec990eed0b233d61e4617bc27
Martoons109 <93096998+Martoons109@users.noreply.github.com>
2021-11-08 17:17:22 +0100

    Add files via upload

---
+++ tempodivita.py
@@ -0,0 +1,7 @@
+print("vuoi sapere quanto hai vissuto?")
+print("ora inserisci la tua età")
+age = int(input("età: "))
+days = age * 365
+minutes = age * 525948     
+seconds = age * 31556926
+print("sei stato in vita per", days, "giorni", minutes, "minuti e", seconds, "secondi!  Wow!")
